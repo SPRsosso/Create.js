182f7492731947a920694ec2480db25b03602dab
sosso <cypossosmu51c@gmail.com>
2025-05-27 09:14:55 +0200

    Added createCircle() to ConvexPolygon class, this method has accuracy (how many points you want to have)

--- ts/main.ts
+++ ts/main.ts
@@ -10,10 +10,33 @@ if (canvas) {
         .backgroundColor("black")
         .resizeCanvas(innerWidth, innerHeight);
 
-    const rect = CreateJS.ConvexPolygon.createRect(100, 100, 100, 100).fillColor("skyblue").fill();
+    const rect = CreateJS.ConvexPolygon.createRect(100, 100, 100, 100).fill();
+    const circle = CreateJS.ConvexPolygon.createCircle(500, 500, 100, 18).strokeWidth(3).stroke();
+
+    const keyboardHandler = new CreateJS.KeyboardEvent.Handler();
+    keyboardHandler.handle(fps);
+    keyboardHandler.register(CreateJS.KeyboardEvent.Key.ArrowLeft, () => {
+        circle.rotate(CreateJS.Math.degToRad(-1));
+    });
+    keyboardHandler.register(CreateJS.KeyboardEvent.Key.ArrowRight, () => {
+        circle.rotate(CreateJS.Math.degToRad(1));
+    });
+    keyboardHandler.register(CreateJS.KeyboardEvent.Key.ArrowUp, () => {
+        circle.scaleFrom(1.01);
+    });
+    keyboardHandler.register(CreateJS.KeyboardEvent.Key.ArrowDown, () => {
+        circle.scaleFrom(0.99);
+    });
 
     CreateJS.TimeHandler.tick(fps, ( currentTick, dt ) => {
-        game.render([ rect ]);
+        if (circle.intersectsWith(rect)) {
+            rect.fillColor("yellow");
+            circle.strokeColor("lime");
+        } else {
+            rect.fillColor("skyblue");
+            circle.strokeColor("red");
+        }
+        game.render([ rect, circle ]);
     });
 }
 
